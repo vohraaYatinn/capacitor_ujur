8b2c55075cd2bebb3ef07604e22418077552ad53
vohraaYatinn <vohrayatin9@gmail.com>
2024-04-29 22:29:53 +0530

    g

--- ujur_patient_app/src/index.js
+++ ujur_patient_app/src/index.js
@@ -5,10 +5,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Store from './redux/Store';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { App as CapacitorApp } from '@capacitor/app';
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+CapacitorApp.addListener('backButton', ({canGoBack}) => {
+  if(!canGoBack){
+    CapacitorApp.exitApp();
+  } else {
+    window.history.back();
+  }
+  });
 root.render(
   <Provider store={Store}>
   <React.StrictMode>
